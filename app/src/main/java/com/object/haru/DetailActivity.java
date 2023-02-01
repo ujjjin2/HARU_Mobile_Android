e3d761d975f9ac8987a6fd70d99d461e6484f364
ujjjin2 <ujin7339@naver.com>
2023-02-02 01:52:06 +0900

    상세페이지 툴바

--- app/src/main/java/com/object/haru/DetailActivity.java
+++ app/src/main/java/com/object/haru/DetailActivity.java
@@ -1,8 +1,13 @@
 package com.object.haru;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //툴바 생성
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//튀로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            }
+            case R.id.action_item1: {
+//                Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
+//                startActivity(intent);
+//                break;
+            }
+
+            case R.id.action_item2: {
+//                Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
+//                startActivity(intent);
+//                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
