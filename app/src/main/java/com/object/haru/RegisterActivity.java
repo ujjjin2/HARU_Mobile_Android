28ebc5c668c02a1ca187a59989f4308a6e00cd07
ujjjin2 <ujin7339@naver.com>
2023-02-02 13:12:36 +0900

    구인공고

--- app/src/main/java/com/object/haru/RegisterActivity.java
+++ app/src/main/java/com/object/haru/RegisterActivity.java
@@ -1,8 +1,11 @@
 package com.object.haru;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //툴바 생성
+        Toolbar toolbar = findViewById(R.id.Register_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//튀로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        getSupportActionBar().setTitle("구인 등록");
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: { //toolbar의 back키 눌렀을 때 동작
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            }
+        }
+            return super.onOptionsItemSelected(item);
+        }
 }
