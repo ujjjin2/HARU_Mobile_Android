a85d374170f038b71e46c711d1e084d449777bde
ujjjin2 <ujin7339@naver.com>
2023-02-09 22:46:46 +0900

    흐음

--- app/src/main/java/com/object/haru/Activity/DetailActivity.java
+++ app/src/main/java/com/object/haru/Activity/DetailActivity.java
@@ -5,25 +5,90 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.object.haru.Activity.MainActivity;
+import com.object.haru.DTO.RecruitDTO;
 import com.object.haru.R;
+import com.object.haru.retrofit.RetrofitClientInstance;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView Detail_tv_writeTime, //작성시간
+            Detail_tv_title, //제목
+            Detail_tv_name, //작성자
+            detail_three_pay2,Detail_tv_pay2, // 최저시급, 최저시급(총)
+            detail_three_date2,Detail_tv_date2, //근무일자
+            detail_three_time2, Detail_tv_time2,//근무시간
+            Detail_tv_category2, //분야
+            Detail_tv_storeinfo2, //매장정보
+            Detail_tv_age2, //우대나이
+            Detail_tv_career2, //우대경력
+            Detail_tv_sex2; //우대 성별
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        Detail_tv_writeTime = findViewById(R.id.Detail_tv_writeTime);
+        Detail_tv_name = findViewById(R.id.Detail_tv_name);
+        Detail_tv_title = findViewById(R.id.Detail_tv_title);
+        detail_three_pay2 = findViewById(R.id.detail_three_pay2); Detail_tv_pay2 = findViewById(R.id.Detail_tv_pay2);
+        detail_three_date2 = findViewById(R.id.detail_three_date2); Detail_tv_date2 = findViewById(R.id.Detail_tv_date2);
+        detail_three_time2 = findViewById(R.id.detail_three_time2); Detail_tv_time2 = findViewById(R.id.Detail_tv_time2);
+        Detail_tv_category2 = findViewById(R.id.Detail_tv_category2);
+        Detail_tv_storeinfo2 = findViewById(R.id.Detail_tv_storeinfo2);
+        Detail_tv_age2 = findViewById(R.id.Detail_tv_age2);
+        Detail_tv_career2 = findViewById(R.id.Detail_tv_career2);
+        Detail_tv_sex2 = findViewById(R.id.Detail_tv_sex2);
+
+
         //툴바 생성
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//튀로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
         getSupportActionBar().setTitle("");
+
+        fetch();
+    }
+
+    private void fetch() {
+        Call<RecruitDTO> call = RetrofitClientInstance.getApiService().getDetailRecruit(1);
+        call.enqueue(new Callback<RecruitDTO>() {
+            @Override
+            public void onResponse(Call<RecruitDTO> call, Response<RecruitDTO> response) {
+                if (response.isSuccessful()){
+                    RecruitDTO recruit = response.body();
+//                    Detail_tv_writeTime.setText(recruit.w);
+//                            Detail_tv_title, //제목
+//                            Detail_tv_name, //작성자
+//                            detail_three_pay2,Detail_tv_pay2, // 최저시급, 최저시급(총)
+//                            detail_three_date2,Detail_tv_date2, //근무일자
+//                            detail_three_time2, Detail_tv_time2,//근무시간
+//                            Detail_tv_category2, //분야
+//                            Detail_tv_storeinfo2, //매장정보
+//                            Detail_tv_age2, //우대나이
+//                            Detail_tv_career2, //우대경력
+//                            Detail_tv_sex2;
+
+
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RecruitDTO> call, Throwable t) {
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/object/haru/Activity/RegisterActivity.java
+++ app/src/main/java/com/object/haru/Activity/RegisterActivity.java
@@ -4,10 +4,14 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,8 +26,12 @@ public class RegisterActivity extends AppCompatActivity {
 
     Button category_btn,register_sp_time1,register_sp_time2;
     Dialog dialogtime1,dialogtime2;
-    EditText year,month,day,hour,min;
+    EditText year,month,day,hour,min,addr;
     TextView dialogtime_title;
+    private Handler handler;
+    private WebView webView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,10 +69,10 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 dialogtime1.show();
                 year = dialogtime1.findViewById(R.id.dialogtime_year);
-                month = dialogtime1.findViewById(R.id.dialogtime_hour);
-                day = dialogtime1.findViewById(R.id.dialogtime_minute);
-                hour = dialogtime1.findViewById(R.id.dialogtime_month);
-                min = dialogtime1.findViewById(R.id.dialogtime_day);
+                month = dialogtime1.findViewById(R.id.dialogtime_month);
+                day = dialogtime1.findViewById(R.id.dialogtime_day);
+                hour = dialogtime1.findViewById(R.id.dialogtime_hour);
+                min = dialogtime1.findViewById(R.id.dialogtime_minute);
 
                 Button btnok = dialogtime1.findViewById(R.id.button2);
                 btnok.setOnClickListener(new View.OnClickListener() {
@@ -122,7 +130,43 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        addr = findViewById(R.id.register_pt_storeinfo);
+
+        //WebView 초기화
+       init_webView();
+        // 핸들러를 통한 JavaScript 이벤트 반응
+        handler = new Handler();
+
+
     }
+
+    private void init_webView() {
+        //webview 설정
+        webView = findViewById(R.id.webView);
+        //자바스크립트의 window.open허용
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        //자바스크립트 이벤트에 대응할 함수를 정의한 클래스를 붙여줌
+        webView.addJavascriptInterface(new AndroidBridge(),"TestApp");
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("");
+
+    }
+
+    private class AndroidBridge {
+        @JavascriptInterface
+        public void setAddress(final String arg1, final String arg2, final String arg3) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    addr.setText(String.format("(%s) %s %s", arg1, arg2, arg3));
+
+                    // WebView를 초기화 하지않으면 재사용할 수 없음
+                    init_webView();
+                }
+            });
+        }
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
